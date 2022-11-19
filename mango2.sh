c27398402bfe80cd5a5aca5cbb6cd932ed108da1
MangoLoveVV <118524176+MangoLoveVV@users.noreply.github.com>
2022-11-19 17:20:13 +0800

    2
    2

--- mango2.sh
+++ mango2.sh
@@ -1,13 +1,14 @@
 data=/d/data_
+# 2.准备data.list
 exec 3<$data/wav
 exec 4<$data/text
 exec 5<$data/text
-rm ‐rf $data/data_list
+rm -rf $data/data.list
 while read wav <&3 && read txt <&4 && read txt1 <&5
-do
-key=`echo $wav | awk ‐F ' ' '{ printf $1}'`
-wav=`echo $wav | awk ‐F ' ' '{ printf $2}'`
-txt=`echo $txt | awk ‐F ' ' '{ printf $2}'`
-echo "{\"key\":\"${key}\",\"wav\":\"${wav}\",\"txt\":\"${txt}\" }" >> $data/data_list
+do  
+    key=`echo $wav | awk -F ' ' '{ printf $1}'`
+    wav=`echo $wav | awk -F ' ' '{ printf $2}'`
+    txt=`echo $txt | awk -F ' ' '{ printf $2}'`
+    echo "{\"key\":\"${key}\",\"wav\":\"${wav}\",\"txt\":\"${txt}\" }" >> $data/data.list
 done
-echo "data_list done!"
+echo "data.list done!"
