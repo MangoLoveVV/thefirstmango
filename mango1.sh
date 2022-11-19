5148d0f8373604431db815ba4e1ad5ccafe5c455
MangoLoveVV <118524176+MangoLoveVV@users.noreply.github.com>
2022-11-19 15:52:22 +0800

    6
    6

--- mango1.sh
+++ mango1.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-# 数据集存放的位置，修改为保存文件的位置
-sample_data=/D/AISHELL-1_sample/S0150
-# 指定新创建文件保存的地方
-data=/D/AISHELL-1_sample/S0150/data
-if [[ ! ‐d $data ]];then
-mkdir ‐p $data
+. ./path.sh
+
+# 数据集存放的位置
+sample_data=/d/AISHELL-1_sample
+# 数据生成的地方
+data=/d/data_
+if [ ! -d $data ];then
+    mkdir -p $data
 fi
 # 初始化
-rm ‐rf $data/wav
-rm ‐rf $data/text
-# 1.准备 wav text
+rm -rf $data/wav.scp
+rm -rf $data/text
+# 1.准备 wav.scp text
 for sub_dir in `ls ${sample_data}`;do
-wav_txt_dir=${sample_data}/${sub_dir}/${sub_dir}_mic
-echo $wav_txt_dir
-for file in `ls $wav_txt_dir`;do
-if [ ${file#*.} != "txt" ];then
-# 准备 wav
-echo "${file%.*} $wav_txt_dir/${file%.*}.wav" >> $data/wav
-# echo `wc ‐l $data/wav`
-# 准备 text
-txt=`cat $wav_txt_dir/${file%.*}.txt`
-echo "${file%.*} $txt" >> $data/text
-fi
+    wav_txt_dir=${sample_data}/${sub_dir}/${sub_dir}_mic
+    echo $wav_txt_dir
+    for file in `ls $wav_txt_dir`;do
+        if [ ${file#*.} != "txt" ];then
+            # 准备wav.scp
+            echo "${file%.*} $wav_txt_dir/${file%.*}.wav" >> $data/wav.scp
+            # echo `wc -l $data/wav.scp`
+            # 准备text
+            txt=`cat $wav_txt_dir/${file%.*}.txt`
+            echo "${file%.*} $txt" >> $data/text
+        fi
+    done
 done
-done
-echo "wav and text done"
+echo "wav.scp and text done!"
